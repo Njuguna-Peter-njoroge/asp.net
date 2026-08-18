12cfb25057402c86faa3cfed7ffdc813705f41a3
Peter Njoroge <njugunahpeternjoroge@gmail.com>
2026-08-18 06:14:01 +0300

    perfomed a successful migration and solved the sqlite framework error

--- GamStore.Api/Program.cs
+++ GamStore.Api/Program.cs
@@ -5,6 +5,11 @@ using GameStore.Api.Dtos;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddValidation();
+
+var connString = "Host=localhost;Port=5432;Database=GamesDb;Username=postgres;Password=postgres";
+builder.Services.AddNpgsql<GameStoreContext>(connString);
+
+
 var app = builder.Build();
 
 app.MapGamesEndpoints();
---
+++ SqliteTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+
+try
+{
+    using var connection = new SqliteConnection("Data Source=Games.Db");
+    connection.Open();
+
+    Console.WriteLine("SQLite connection successful!");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("SQLite failed:");
+    Console.WriteLine(ex.ToString());
+}
